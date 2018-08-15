9c6bc2623e7feeabfcdb58e1dbb5a983202317a6
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2018-08-15 15:47:59 -0700

    1.2.3 golang idiom for parallel processing.

--- src/chat/room.go
+++ src/chat/room.go
@@ -6,3 +6,26 @@ type room struct {
 	leave   chan *client     // a channel for a client that will leave chat room.
 	clients map[*client]bool // hold clients that are joining the chat room.
 }
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// Sending message:
+				default:
+					// Failed sending message:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
