e284ee77e839997966609fbe8f655221799bc503
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2018-08-15 14:17:04 -0700

    1.1.1 Template

--- src/chat/main.go
+++ src/chat/main.go
@@ -3,21 +3,26 @@ package main
 import (
 	"net/http"
 	"log"
+	"sync"
+	"text/template"
+	"path/filepath"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					<p>Let's chatting!!</p>
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	once sync.Once
+	filename string
+	templ *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil) // Should check return value in actual project.
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
