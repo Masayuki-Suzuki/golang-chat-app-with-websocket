dca98a50a9865ddbb4d8566c2694ea97f63c5e79
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2018-08-26 14:29:29 -0700

    2.6.2 Response interpretation from auth provider.

--- src/chat/auth.go
+++ src/chat/auth.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
 )
 
 type authHandler struct {
@@ -45,6 +46,27 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			log.Fatalln("Error!! Couldn't call GetBeginAuthURL function.", provider, "-", err)
 		}
 		w.Header().Set("Location", loginUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("Getting authentication provider failed: ", provider, "-", err)
+		}
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			log.Fatalln("Couldn't complete authentication.", provider, "-", err)
+		}
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			log.Fatalln("Failed getting user data.", provider, "-", err)
+		}
+		authCookieValue := objx.New(map[string]interface{}{"name": user.Name()}).MustBase64()
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: authCookieValue,
+			Path:  "/",
+		})
+		w.Header()["Location"] = []string{"/chat"}
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		w.WriteHeader(http.StatusNotFound)
